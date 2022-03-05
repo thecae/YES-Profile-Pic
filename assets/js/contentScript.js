aa8388a81f1d22b5006389c1dc3f2f83f361894d
Cole <bleeper_genius0c@icloud.com>
2022-03-05 00:42:05 -0600

    Created YES Profile

---
+++ assets/js/contentScript.js
@@ -0,0 +1,4 @@
+console.log("YES Profile - Content Script is Running!");
+
+let image = document.getElementById("photo");
+image.src = "https://cdn.discordapp.com/attachments/747649164116099082/948996260889776128/Webp.net-resizeimage-2.png";
